d32c1ce19689bf6d66570d73d3e64a28cb04d0c2
NoorCEO <37270700+NoorCEO@users.noreply.github.com>
2020-12-02 21:52:35 +0200

    Add files via upload

---
+++ encrypt rail fence.py
@@ -0,0 +1,26 @@
+# ==============================================================================
+#
+#    Use:
+#    decrypt("Hello World", 4)
+#    => "HWe o!lordll"
+#
+# ==============================================================================
+
+def encrypt(s,n):
+  fence = [[] for i in range(n)]
+  rail  = 0
+  var   = 1
+
+  for char in s:
+    fence[rail].append(char)
+    rail += var
+
+    if rail == n-1 or rail == 0:
+      var = -var
+
+  res = ''
+  for i in fence:
+    for j in i:
+      res += j
+
+  return res
